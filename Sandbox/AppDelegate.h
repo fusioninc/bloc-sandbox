1a227f400d336b7fd379b1ab324af0de53fe9b37
Jason Kuang <jason.jkuang@gmail.com>
2015-05-27 16:18:41 -0700

    first commit

---
+++ Sandbox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sandbox
+//
+//  Created by Fusion on 5/27/15.
+//  Copyright (c) 2015 Jason Kuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sandbox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sandbox
+//
+//  Created by Fusion on 5/27/15.
+//  Copyright (c) 2015 Jason Kuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
